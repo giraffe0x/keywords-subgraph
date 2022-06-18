72be02b05c2c809c1f579dce5efa2da94a1766c7
Nicholas Chew <chewonithard@gmail.com>
2022-06-18 18:11:47 +0800

    first commit

--- src/sender-token.ts
+++ src/sender-token.ts
@@ -1,96 +1,55 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  SenderToken,
-  ApprovalForAll,
-  OwnershipTransferred,
   SenderBatchTransfer,
   SenderBurn,
-  SenderContractChange,
   SenderInitialMint,
   SenderInitialVerifiedMint,
   SenderMint,
-  SenderTransfer,
-  TransferBatch,
-  TransferSingle,
-  URI
+  SenderTransfer
 } from "../generated/SenderToken/SenderToken"
-import { ExampleEntity } from "../generated/schema"
 
-export function handleApprovalForAll(event: ApprovalForAll): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import { SenderToken, User, UserTokenBalance } from "../generated/schema"
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+export function handleSenderInitialVerifiedMint(event: SenderInitialVerifiedMint): void {
+  const userId = event.params.to.toHexString()
+  const tokenId = event.params.tokenId.toString();
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  const userTokenBalance = new UserTokenBalance(`${userId}-${tokenId}`);
+  userTokenBalance.user = userId;
+  userTokenBalance.senderToken = tokenId;
+  userTokenBalance.balance = event.params.amount;
+  userTokenBalance.save();
+
+  let user = User.load(userId);
+  if (!user) {
+    user = new User(userId);
+    user.save();
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-  entity.operator = event.params.operator
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract._exists(...)
-  // - contract.balanceOf(...)
-  // - contract.balanceOfBatch(...)
-  // - contract.getUri(...)
-  // - contract.idToVerifiedOwner(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.mintPrice(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.paused(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenSupply(...)
-  // - contract.uri(...)
+  let token = SenderToken.load(tokenId)
+  if(!token) {
+    token = new SenderToken(tokenId);
+    token.tokenID = event.params.tokenId;
+    token.keyword = event.params.keyword;
+    token.save();
+  }
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+// for existing sender tokens
+export function handleSenderMint(event: SenderMint): void {
+  const userId = event.params.to.toHexString()
+  const tokenId = event.params.tokenId.toString();
+  let userTokenBalance = UserTokenBalance.load(`${userId}-${tokenId}`)
 
-export function handleSenderBatchTransfer(event: SenderBatchTransfer): void {}
+  // create if does not exist
+  if (!userTokenBalance) {
+    userTokenBalance = new UserTokenBalance(`${userId}-${tokenId}`);
+    userTokenBalance.user = userId;
+    userTokenBalance.senderToken = tokenId;
+    userTokenBalance.balance = event.params.amount;
+  } else {
+    // update new balance
+    userTokenBalance.balance = userTokenBalance.balance.plus(event.params.amount);
+  }
 
-export function handleSenderBurn(event: SenderBurn): void {}
-
-export function handleSenderContractChange(event: SenderContractChange): void {}
-
-export function handleSenderInitialMint(event: SenderInitialMint): void {}
-
-export function handleSenderInitialVerifiedMint(
-  event: SenderInitialVerifiedMint
-): void {}
-
-export function handleSenderMint(event: SenderMint): void {}
-
-export function handleSenderTransfer(event: SenderTransfer): void {}
-
-export function handleTransferBatch(event: TransferBatch): void {}
-
-export function handleTransferSingle(event: TransferSingle): void {}
-
-export function handleURI(event: URI): void {}
+  userTokenBalance.save();
+}
